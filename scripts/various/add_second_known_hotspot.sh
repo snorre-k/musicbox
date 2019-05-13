0e99fa8561dcc2a8bec5a830d945b266fb2ed20d
snorre-k <norbert.kipp@snorre.com>
2019-05-13 21:33:09 +0200

    2nd WIFI

---
+++ scripts/various/add_second_known_hotspot.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+## Add a hotspot to the WIFI configuration - is used, when main WIFI is unavailable
+## Can be a personal / tethering hotspot (e.g. smartphone)
+
+# Import Helpers
+DIR=`dirname $0`
+pushd $DIR > /dev/null
+. ./helpers.sh
+popd > /dev/null
+
+# Check User
+check_user_ability
+
+# 2nd WIFI
+echo -e "$INFO Add a hotspot to the WIFI configuration"
+echo    "      This WIFI is used when main WIFI is unavailable"
+echo    "      You can use a personal / tethering hotspot (e.g. smartphone)"
+echo -n "Network SSID: "
+read SSID
+echo -n "Network password: "
+read PASSWORD
+
+if [ "$SSID" -a "$PASSWORD" ]; then
+  # Set existing WIFI to prio1
+  sudo sed -i '/ssid=/a \tpriority=1' /etc/wpa_supplicant/wpa_supplicant.conf
+  wpa_add=`/usr/bin/wpa_passphrase "$SSID" "$PASSWORD" | grep -v "#" | sed "/ssid=/a \        priority=2"`
+  sudo echo >> /etc/wpa_supplicant/wpa_supplicant.conf
+  sudo echo "$wpa_add" >> /etc/wpa_supplicant/wpa_supplicant.conf
+else
+  echo -e "$ERROR SSID or PSK not given - not configuring 2nd WIFI"
+fi
