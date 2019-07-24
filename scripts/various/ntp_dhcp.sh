7f1665beb66a01436ac45d7251aa6bd2dc41083b
snorre-k <norbert.kipp@snorre.com>
2019-07-24 19:02:59 +0200

    Bugfix - Escape $

--- scripts/various/ntp_dhcp.sh
+++ scripts/various/ntp_dhcp.sh
@@ -17,19 +17,19 @@ cat << EOF  | sudo tee /lib/dhcpcd/dhcpcd-hooks/50-timesyncd.conf > /dev/null
 confd=/run/systemd/timesyncd.conf.d
 
 set_servers() {
-    mkdir -p "$confd"
+    mkdir -p "\$confd"
     (
         echo "# Created by dhcpcd hook"
         echo "[Time]"
-        echo "NTP=$new_ntp_servers"
-    ) > "$confd/dhcp-ntp.conf"
+        echo "NTP=\$new_ntp_servers"
+    ) > "\$confd/dhcp-ntp.conf"
 
     # Tell timesyncd it has an updated configuration
     systemctl try-reload-or-restart systemd-timesyncd
 }
 
-if $if_up; then
-    if [ "$new_ntp_servers" ]; then
+if \$if_up; then
+    if [ "\$new_ntp_servers" ]; then
         set_servers
     fi
 fi
